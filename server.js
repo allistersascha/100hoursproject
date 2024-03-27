949c7d5ec5cd50e545e96cdbb4b8db8ec624b9ff
Allister Sascha <101225242+allistersascha@users.noreply.github.com>
2024-03-27 15:22:17 -0500

    look ma made a server

---
+++ server.js
@@ -0,0 +1,52 @@
+
+
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const flash = require('express-flash')
+const logger = require('morgan')
+const connectDB = require('./config/database')
+const mainRoutes = require('./routes/main')
+const artistdashRoutes = require('./routes/artistdash')
+const artistpageRoutes = require('./routes/artistpage')
+require('dotenv').config({path: './config/.env'})
+
+
+require('./config/passport')(passport)
+
+connectDB()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(logger('dev')) 
+
+app.use(
+    session({
+      secret: 'keyboard cat',
+      resave: false,
+      saveUninitialized: false,
+      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    })
+  )
+  
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(flash())
+  
+app.use('/', mainRoutes)
+app.use('/artistdash', artistdashRoutes)
+app.use('/artistpage', artistpageRoutes)
+ 
+app.listen(process.env.PORT, ()=>{
+    console.log('Server is running, you better catch it!')
+})    
+
+
+//this can't be everything unless mvc is just that magical
