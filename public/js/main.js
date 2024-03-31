9772e29435e151fb236b821634b2f14eca088dd7
Allister Sascha <101225242+allistersascha@users.noreply.github.com>
2024-03-31 15:49:28 -0500

    like a todo list of event listeners

--- public/js/main.js
+++ public/js/main.js
@@ -156,3 +156,13 @@ async function addSong(){
         console.log(err)
     }
 }
+
+const playIconCont = document.getElementById("playIcon");
+const muteIconCont = document.getElementById("muteIcon");
+const audioPlayerCont = document.getElementById("audioContainer");
+const seekSlider = document.getElementById("seekSlider");
+const volumeSlider = document.getElementById("volumeSlider")
+const audio = document.querySelector('audio');
+const durationCont = document.getElementById('duration');
+const currentTimeCont = document.getElementById('currentTime');
+const outputCont = document.getElementById('volumeOutput');
